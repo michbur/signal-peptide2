213c55339a87d8a2b66baf80ac45ad4048342233
michbur <michalburdukiewicz@gmail.com>
2015-01-14 20:37:32 +0100

    preliminary calculations succesfully finished

--- cleave_prediction/cross_val.R
+++ cleave_prediction/cross_val.R
@@ -102,5 +102,5 @@ multifolds_cl_work <- pblapply(1L:4, function(dummy_variable) {
 
 #stopCluster(cl)
 
-save(multifolds_cl_work, file = "cleave_pred40.RData")
+save(multifolds_cl_work, file = "cleave_pred4.RData")
 
