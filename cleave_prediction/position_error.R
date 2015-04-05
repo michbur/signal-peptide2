620a990e7ba256971aba02c8e236af1a0a08410d
michbur <michalburdukiewicz@gmail.com>
2015-04-05 23:03:41 +0200

    added some preliminary calculations for prediction of cleavage site position

---
+++ cleave_prediction/position_error.R
@@ -0,0 +1,32 @@
+load(paste0(pathway, "cleave_pred80names.RData")) -> tmp
+names80 <- multifolds_cl_work
+
+load(paste0(pathway, "cleave_pred170names.RData")) -> tmp
+names170 <- multifolds_cl_work
+
+multifolds_cl_work <- c(names80, names170)
+rm(names80, names170)
+
+
+
+#calculate mean error of position prediction
+
+prot_names <- aggregate(Freq ~ all_cv, 
+                        do.call(rbind, lapply(multifolds_cl_work, function(single_fold) {
+                          all_cv <- unlist(lapply(single_fold, function(single_cv) {
+                            sub_dat <- single_cv[!is.na(single_cv[, "real"]),
+                                                 c("sp_probability", "real")]
+                            rownames(sub_dat[sub_dat[, "sp_probability"] < 0.5, ])  
+                          }))
+                          data.frame(table(all_cv))
+                        })), sum)
+
+
+sqrt(mean(sapply(multifolds_cl_work, function(single_fold) {
+  all_cv <- do.call(rbind, lapply(single_fold, function(single_cv) {
+    sub_dat <- single_cv[!is.na(single_cv[, "real"]),
+                         c("sp_end", "real")]
+  }))
+  mean((all_cv[, "sp_end"] - all_cv[, "real"])^2)
+})))
+#average position misplacement - 4.14 aa
