1ed89f12cc2a967ea0a3e2d152a62b22a6ede275
michbur <michalburdukiewicz@gmail.com>
2015-04-20 02:56:07 +0200

    added comparision data (normal n-grams)

---
+++ building_ngrams/normal_ngrams_comparision.R
@@ -0,0 +1,78 @@
+pos_seqs <- read_uniprot(paste0(pathway, "sept_signal.txt"), euk = TRUE)
+neg_seqs <- read.fasta(paste0(pathway, "sept_neg.fasta"), seqtype = "AA")
+#remove sequences with atypical aminoacids
+atyp_aa <- which(sapply(neg_seqs, function(i) any(i %in% c("X", "J", "Z", "B", "U"))))
+too_short <- which(sapply(neg_seqs, length) < 80)
+neg_seqs <- neg_seqs[-unique(c(atyp_aa, too_short))]
+
+too_short <- which(sapply(pos_seqs, length) < 80)
+pos_seqs <- pos_seqs[-c(too_short)]
+#here remove too short signal peptides
+pos_seqs <- pos_seqs[sapply(pos_seqs, function(i) attr(i, "sig")[2]) > 15]
+
+
+cleaves <- tolower(t(sapply(pos_seqs, function(seq) {
+  cleave_site <- attr(seq, "sig")[2]
+  seq[(cleave_site-4):(cleave_site+3)]
+})))
+
+pre_cleaves <- tolower(t(sapply(pos_seqs, function(seq) {
+  cleave_site <- attr(seq, "sig")[2]
+  seq[(cleave_site-11):(cleave_site-4)]
+})))
+
+post_cleaves <- tolower(t(sapply(pos_seqs, function(seq) {
+  cleave_site <- attr(seq, "sig")[2]
+  seq[(cleave_site+3):(cleave_site+10)]
+})))
+
+tar <- c(rep(1, nrow(cleaves)), rep(0, 2*nrow(cleaves)))
+
+all_data <- degenerate(matrix(l2n(rbind(cleaves, post_cleaves, pre_cleaves), 
+                                  seq_type = "prot"), ncol = 8),
+                       list(`1` = c(1, 6, 8, 10, 11, 18),
+                            `2` = c(2, 13, 14, 16, 17),
+                            `3` = c(5, 19, 20),
+                            `4` = c(7, 9, 12, 15),
+                            `4` = c(3, 4)))
+
+pos_id <- which(tar == 1)
+neg_id <- which(tar == 0)
+
+
+
+#here fold lapply
+
+res_norm <- pblapply(1L:4, function(repetition) {
+  folds_pos <- cvFolds(length(pos_id), type = "random")
+  folds_neg <- cvFolds(length(neg_id), type = "random")
+  lapply(1L:5, function(fold) {
+    pos_train <- folds_pos[["subsets"]][folds_pos[["which"]] == fold]
+    pos_test <- folds_pos[["subsets"]][folds_pos[["which"]] != fold]
+    
+    neg_train <- folds_neg[["subsets"]][folds_neg[["which"]] == fold]
+    neg_test <- folds_neg[["subsets"]][folds_neg[["which"]] != fold]
+    
+    trigrams <- cut(test_features(c(rep(1, length(pos_train)), 
+                                rep(0, length(neg_train))), 
+                              count_ngrams(all_data[c(pos_train, neg_train), ], 
+                                           u = 1L:5, n = 3, pos = TRUE)), 
+                    breaks = c(0, 0.05, 1))[[1]]
+    
+    teragrams <- cut(test_features(c(rep(1, length(pos_train)), 
+                                 rep(0, length(neg_train))), 
+                               count_ngrams(all_data[c(pos_train, neg_train), ], 
+                                            u = 1L:5, n = 4, pos = TRUE)), 
+                     breaks = c(0, 0.05, 1))[[1]]
+    
+    tri_models <- train_models(pos_train, neg_train, all_data, trigrams)
+    tetra_models <- train_models(pos_train, neg_train, all_data, teragrams)
+    cbind(fold = rep(fold, 6), 
+          n = c(rep(3, 3), rep(4, 3)),
+          test_models(pos_test, neg_train, all_data, trigrams, tri_models),
+          test_models(pos_test, neg_test, all_data, teragrams, tetra_models))
+    
+  })
+})
+
+save(res_norm, file = paste0(pathway, "build_cleave_norm.RData"))
